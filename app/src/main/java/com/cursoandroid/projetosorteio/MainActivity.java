4a12fddb42eedf3bc5ff5b29d7f4195dc4686833
fernandomx <fernando973@gmail.com>
2020-04-20 23:53:55 -0300

    Start Commit!

---
+++ app/src/main/java/com/cursoandroid/projetosorteio/MainActivity.java
@@ -0,0 +1,27 @@
+package com.cursoandroid.projetosorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void SortearNumero(View view){
+        TextView texto = findViewById(R.id.textoNumeroSelecionado);
+
+        int x = new Random().nextInt(11);
+
+        texto.setText("Numero:" + x);
+
+    }
+}
